17048bdd53322d73ae1342ab60db98a36f8bd89b
1412180203 <940710020@qq.com>
2017-03-14 22:30:40 +0800

    改

--- src/main/webapp/resource/js/module/user/home.js
+++ src/main/webapp/resource/js/module/user/home.js
@@ -12,6 +12,33 @@ define(['jquery'],function($){
 	
 	var main = function(){
 		handle_event();
+		$(function() {
+			var width = $(window).width();
+			var height = $(document).height();
+			$("#left").height(height);
+			$("iframe").css("width",width-180).css("height",height-60);
+			
+			$(".nav li").click(function() {
+				var index = $(this).index();
+				$(this).addClass("select").siblings().removeClass("select");
+				$("#left>nav>ul").hide();
+				$("#left>nav>ul:eq("+index+")").show();
+			});
+			
+			$("#left nav>ul>li>ul").before("<img src='image/right.png' class='arrow' />");
+			
+		    $("#left nav>ul>li>ul").parent().click(function() {
+				if($(this).children("ul").is(":visible")) {
+					$(this).children("ul").slideUp(500).parent().css("padding-bottom","6px").children("img.arrow").attr("src","image/right.png");
+				} else {
+					$(this).children("ul").slideDown(500).parent().css("padding-bottom", "0").children("img.arrow").attr("src","image/down.png");
+				}
+			});
+			
+			$("#left li ul li").click(function(event) {
+				event.stopPropagation();
+			})
+		});
 	}
 	
 	// 监听事件的处理
